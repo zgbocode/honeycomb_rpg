d95458db1afd89377fee540c10369aab096c564f
Emmanuel Ezeigbo <ezeigboemmanuel2@gmail.com>
2025-07-15 12:44:20 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        // add more tokens if you're using them in @apply
+      },
+    },
+  },
+  plugins: [],
+};
